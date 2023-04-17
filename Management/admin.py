ae22b263fbccbbea4ddbbbbb0dc34695489df8cd
candr <andreas7@uwm.edu>
2023-04-17 13:06:46 -0500

    made it work, bugs were in admin

--- Management/admin.py
+++ Management/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from models import User, course
+from .models import User, Course
 
 # Register your models here.
 admin.site.register(User)
-admin.site.register(course)
+admin.site.register(Course)
--- Management/models.py
+++ Management/models.py
@@ -1,20 +1,18 @@
-from django.contrib.postgres.fields import ArrayField
 from django.db import models
 
 # Create your models here.
+class Course(models.Model) :
 
+    section = models.IntegerField()
+    #users = models.ManyToManyField(User)
+    #tasks = ArrayField(models.CharField(max_length=20))
 class User(models.Model):
 
     name = models.CharField(max_length=20)
     username = models.CharField(max_length=20)
     password = models.CharField(max_length=20)
-    phone = models.IntegerField
+    phone = models.IntegerField()
     email = models.CharField(max_length=30)
-    role = models.CharField(manx_length=20) #string match later  or models.ManyToManyField(roles))
-    course = models.ManyToManyField(course)
+    role = models.CharField(max_length=20) #string match later  or models.ManyToManyField(roles))
+    course = models.ManyToManyField(Course)
 
-class course(models.Model) :
-
-    section = models.IntegerField()
-    users = models.ManyToManyField(User)
-    tasks = ArrayField(models.CharField(max_length=20))
