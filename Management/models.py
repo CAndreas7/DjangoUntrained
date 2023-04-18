cf2d66e1053b20cc95a6c791065d7a8b5ae4717b
ethannieskes <113068571+ethannieskes@users.noreply.github.com>
2023-04-18 11:54:11 -0500

    Update models.py

--- Management/models.py
+++ Management/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 # Create your models here.
+"""
 class Course(models.Model) :
 
     section = models.IntegerField()
@@ -16,3 +17,64 @@ class User(models.Model):
     role = models.CharField(max_length=20) #string match later  or models.ManyToManyField(roles))
     course = models.ManyToManyField(Course)
 
+"""
+
+
+# This is the User table, which stores information about the users
+# The email (UWM) is the primary key, but the login method should match
+# the password field to the correct email
+class User(models.Model):
+    # specifying primary_key=True disables the default ID field
+    email = models.CharField(max_length=20, primary_key=True)
+    password = models.CharField(max_length=20)
+    phone = models.CharField(max_length=15)
+    # this is a way I found of representing in the DB the role a user is
+    # the person creating the account with add in a 1, 2, or 3
+    # and see Supervisor/Instructor/TA in the DB in the admin page
+    role = models.PositiveSmallIntegerField(
+        choices=(
+            (1, "Supervisor"),
+            (2, "Instructor"),
+            (3, "TA")
+        ))
+
+
+# This is the Course table, which stores information about a Course at a university
+# The only necessary field is: courseID
+class Course(models.Model):
+    # specifying primary_key=True disables the default ID field
+    courseID = models.IntegerField(primary_key=True)
+    courseName = models.CharField(max_length=40)
+    # VVVV  These 2 fields below may not be necessary, included for appearance
+    courseDescription = models.CharField(max_length=140)
+    courseDepartment = models.CharField(max_length=16)
+
+
+# This is the Section table, to reference a LAB section for a course.
+# The only NECESSARY fields are:  sectionID, TA, and courseID
+#
+class Section(models.Model):
+    sectionID = models.IntegerField(primary_key=True)
+    # building and room number like EMS180
+    location = models.CharField(max_length=8)
+    # these 2 would be formatted "11:00AM
+    startTime = models.CharField(max_length=7)
+    endTime = models.CharField(max_length=7)
+    capacity = models.IntegerField()
+    # this references a value in Courses, when a TA/User is deleted
+    # the section will remain and this field becomes Null
+    TA = models.ForeignKey(User, on_delete=models.SET_NULL())
+    # since this is intrinsically tied to Courses, when a course is deleted
+    # the section shouldn't exist anymore, so we delete this section
+    courseID = models.ForeignKey(Course, on_delete=models.CASCADE())
+
+    # this is a junction table, showing a user assigned to a course
+    # A user can be assigned multiple courses, and a course can have multiple users assigned
+    # this should have an intrinsic, auto incrementing ID field in Django
+    # When a course is deleted, the assignment doesn't exist anymore
+    # When a user is deleted, the assignment doesn't exist anymore
+
+
+class UsersToCourse(models.Model):
+    courseID = models.ForeignKey(Course, on_delete=models.CASCADE())
+    assignment = models.ForeignKey(User, on_delete=models.CASCADE())
