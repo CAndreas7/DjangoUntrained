76ffbceda991375c38920d6c58890c6d9646c7c5
candr <andreas7@uwm.edu>
2023-04-19 17:06:10 -0500

    I think i got the web page to work now, rewrote view and urls and removed a reference to static in main/home.html

--- Management/views.py
+++ Management/views.py
@@ -2,9 +2,35 @@ from django.shortcuts import render, redirect
 from django.views import View
 from .models import User
 
+
 # Create your views here.
+# Need to create a landing page after login
+# replace temp paths with proper url
 
 class Home(View):
 
+    def get(self, request):
+        return render(request, "main/home.html", {})
+
+    def post(self, request):
+        noSuchUser = False
+        badPassword = False
+        try:
+            m = User.objects.get(name=request.POST['email'])
+            badPassword = (m.password != request.POST['password'])
+        except:
+            noSuchUser = True
+        if noSuchUser:
+            return render(request, "home.html", {"message": "User does not exist"})
+        elif badPassword:
+            return render(request, "home.html", {"message": "bad password"})
+        else:
+            request.session["name"] = m.name
+            return redirect("/main/")
+
+
+class MainHome(View):
+
     def get(self,request):
-        return render(request,"home.html", {})
+        m = request.session["name"]
+        return render(request, "main/mainHome.html", {"name": m})
