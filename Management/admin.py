ed07f8388ab96cd7b903c2cde80398c2699ebd18
Adam-Ustby <adamustby@gmail.com>
2023-04-19 15:36:58 -0500

    updated admin page
    added the other models and created a more visually accessible way to see all the different models in the admin page

--- Management/admin.py
+++ Management/admin.py
@@ -1,6 +1,26 @@
 from django.contrib import admin
-from .models import User, Course
+from .models import User, Course, Section, UsersToCourse
+
 
 # Register your models here.
-admin.site.register(User)
-admin.site.register(Course)
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("email", "password", "phone", "role")
+
+
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ("courseID", "courseName", "courseDescription", "courseDepartment")
+
+
+class SectionAdmin(admin.ModelAdmin):
+    list_display = ("sectionID", "location", "startTime", "endTime", "capacity", "TA", "courseID")
+
+
+class UsersToCourseAdmin(admin.ModelAdmin):
+    list_display = ("courseID", "assignment")
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Course, CourseAdmin)
+admin.site.register(Section, SectionAdmin)
+admin.site.register(UsersToCourse, UsersToCourseAdmin)
