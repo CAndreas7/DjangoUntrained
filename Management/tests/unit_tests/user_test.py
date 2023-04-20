48d1b650d39e086fd3539a908041c35c44ad0468
ethannieskes <113068571+ethannieskes@users.noreply.github.com>
2023-04-20 12:03:02 -0500

    Update user_test.py

--- Management/tests/unit_tests/user_test.py
+++ Management/tests/unit_tests/user_test.py
@@ -2,7 +2,8 @@ import unittest
 from Management.models import User
 from django.test import TestCase
 
-class TestUser:
+
+class TestUser(TestCase):
 
     def setUp(self):
         self.supervisor = User("testemail@uwm.edu", "testpassword", "", 1)
@@ -11,31 +12,41 @@ class TestUser:
 
     def test_getUsername(self):
         username = self.supervisor.getUsername()
-        self.assertEquals(username, self.supervisor.email, msg="Returned username was not the user's username")
+        self.assertEqual(username, self.supervisor.email, msg="Returned username was not the user's username")
 
     def test_setUsername(self):
-        pass
+        self.supervisor.setUsername("myemail@uwm.edu")
+        self.assertEqual(self.supervisor.email, "myemail@uwm.edu", msg="Username was not changed")
 
     def test_getPassword(self):
-        pass
+        password = self.supervisor.getPassword()
+        self.assertEqual(self.supervisor.password, password, msg="getPassword did not grab the user's password")
 
     def test_setPassword(self):
-        pass
+        self.supervisor.setPassword("newpassword")
+        self.assertEqual(self.supervisor.password, "newpassword", msg="Password was not changed")
 
     def test_getPhone(self):
-        pass
+        phone = self.supervisor.getPhone()
+        self.assertEqual(self.supervisor.phone, phone, msg="getPhone did not grab the user's phone")
 
     def test_setPhone(self):
-        pass
+        self.supervisor.setPhone("1(111)-111-1111")
+        self.assertEqual(self.supervisor.phone, "1(111)-111-1111", msg="setPhone was did not change user's phone")
 
     def test_addAccount(self):
-        pass
+        self.supervisor.addAccount("email", "pass", "phone", 3)
+        newAcc = User(email="email", password="pass", phone="phone", role=3)
+        self.assertEqual(User.objects.filter(email="email", password="pass"), newAcc)
 
     def test_editAccount(self):
-        pass
+        self.supervisor.editAccount("")
 
     def test_removeAccount(self):
-        pass
+        user = User.objects.create("deleteme@uwm.edu", "goodbye", "", 3)
+        self.supervisor.removeAccount("deleteme@uwm.edu")
+        self.assertEqual(User.objects.filter(email="deleteme@uwm.edu", password="goodbye").len(), 0,
+                         msg="An account exists with the credentials that should have been deleted")
 
     def test_addCourse(self):
         pass
