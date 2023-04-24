abb8539f924df38e1cc55b16e21554dfe679224b
ethannieskes <113068571+ethannieskes@users.noreply.github.com>
2023-04-23 20:50:15 -0500

    Update views.py
    Added in my MyUser class for all of my code.  I want to refactor some of my code and add some more tests for better robustness

--- Management/views.py
+++ Management/views.py
@@ -135,3 +135,97 @@ class editUserInCourse(View):
 class accountEdit(View):
     def get(self, request):
         return render(request, "main/accountEdit.html", {})
+
+    
+class MyUser(User):
+
+    def __init__(self, email, password, phone, role):
+        self.email = email
+        self.password = password
+        self.phone = phone
+        self.role = role
+
+    def addAccount(self, email, password, phone, role):
+        user = User(email=email, password=password, phone=phone, role=role)
+        user.save()
+
+    def getEmail(self):
+        return self.email
+
+    def setEmail(self, email):
+        users = User.objects.filter(email=self.email)
+        for user in users:
+            user.email = email
+            user.save()
+        self.email = email
+
+    def getPassword(self):
+        return self.password
+
+    def setPassword(self, password):
+        passwords = User.objects.filter(password=self.password)
+        for word in passwords:
+            word.password = password
+            word.save()
+        self.password = password
+
+    def getPhone(self):
+        return self.phone
+
+    def setPhone(self, phoneNum):
+        phones = User.objects.filter(phone=phoneNum)
+        for item in phones:
+            item.phone = phoneNum
+            item.save()
+        self.phone = phoneNum
+
+    def editAccount(self, email, password, phoneNum, role):
+        user = User.objects.filter(email=email)
+        for item in user:
+            item.password = password
+            item.phone = phoneNum
+            item.role = role
+            item.save()
+        self.password = password
+        self.phone = phoneNum
+        self.role = role
+
+    def removeAccount(self, email):
+        User.objects.filter(email=email).delete()
+        self.email = None
+        self.password = None
+        self.phone = None
+        self.role = None
+
+    def addCourse(self, courseID, courseName, courseDesc, courseDept):
+        course = Course(courseID=courseID, courseName=courseName, courseDescription=courseDesc, courseDepartment=courseDept)
+        course.save()
+
+    def editCourse(self, courseID, courseName, courseDescription, courseDepartment):
+        courses = Course.objects.filter(courseID=courseID)
+        for item in courses:
+            item.courseName = courseName
+            item.courseDescription = courseDescription
+            item.courseDepartment = courseDepartment
+            item.save()
+
+    def removeCourse(self, courseID):
+        Course.objects.filter(courseID=courseID).delete()
+
+    def addSection(self, sectionID, location, startTime, endTime, capacity, ta, courseID):
+        section = Section(sectionID=sectionID, location=location, startTime=startTime, endTime=endTime, capacity=capacity, TA=ta, courseID=courseID)
+        section.save()
+
+    def editSection(self, sectionID, location, startTime, endTime, capacity, ta, courseID):
+        sections = Section.objects.filter(sectionID=sectionID)
+        for item in sections:
+            item.location = location
+            item.startTime = startTime
+            item.endTime = endTime
+            item.capacity = capacity
+            item.TA_id = ta
+            item.courseID_id = courseID
+            item.save()
+
+    def removeSection(self, sectionID):
+        Section.objects.filter(sectionID=sectionID).delete()
