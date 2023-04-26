2d40ee0c81e5c0a7953517ebac266c36df17ced8
Ethan Nieskes <nieskeee8164@uwec.edu>
2023-04-25 21:48:51 -0500

    Robust tests for User

--- Management/test/unit_tests/user_test.py
+++ Management/test/unit_tests/user_test.py
@@ -49,16 +49,6 @@ class testAccount(TestCase):
         self.supervisor.addAccount("email", "pass", "phone", 3)
         self.assertEqual(User.objects.filter(email="email").count(), 1, msg="No user exists with the given email")
 
-    def test_editAccount(self):
-        # from my understanding, this method should edit the user's own account
-        # when a supervisor selects an account to edit, it selects an account, builds that user, and calls
-        # THAT user's editAccount
-        compAccount = User("taemail@uwm.edu", "newpass", "1(888)-999-0000", 3)
-        compAccount.save()
-        self.supervisor.editAccount("taemail@uwm.edu", "changedpass", "1(888)-999-0000", 3)
-        self.assertEqual(User.objects.filter(password="changedpass").count(), 1,
-                         msg="Editted field did not match desired field")
-
     def test_removeAccount(self):
         user = User(email="deleteme@uwm.edu", password="goodbye", phone="", role=3)
         user.save()
@@ -67,7 +57,28 @@ class testAccount(TestCase):
                          msg="An account exists with the credentials that should have been deleted")
 
 
-class testCourse(TestCase):
+class editAccount(TestCase):
+
+    def setUp(self):
+        self.user = User("testemail@uwm.edu", "testpassword", "", 1)
+        self.user.save()
+        self.supervisor = MyUser("testemail@uwm.edu", "testpassword", "", 1)
+
+    def test_editPassword(self):
+        self.supervisor.editAccount(self.supervisor.email, "changedpass", "1(888)-999-0000", 3)
+        self.assertEqual(User.objects.filter(password="changedpass").count(), 1,
+                         msg="Editted field did not match desired field")
+
+    def test_editPhone(self):
+        self.supervisor.editAccount(self.supervisor.email, self.supervisor.password, "1(449)-844-2323", 1)
+        self.assertEqual(User.objects.filter(phone="1(449)-844-2323").count(), 1, msg="No account with given phone")
+
+    def test_editRole(self):
+        self.supervisor.editAccount(self.supervisor.email, self.supervisor.password, self.supervisor.phone, 2)
+        self.assertEqual(User.objects.filter(role=2).count(), 1, msg="No account with given role number")
+
+
+class testCourseAddRemove(TestCase):
 
     def setUp(self):
         self.user = User("testemail@uwm.edu", "testpassword", "", 1)
@@ -81,13 +92,6 @@ class testCourse(TestCase):
 
         self.assertEqual(Course.objects.filter(courseID=101).count(), 1, msg="No course exists with the given courseID")
 
-    def test_editCourse(self):
-        course = Course(courseID=101, courseName="name", courseDescription="desc", courseDepartment="dept")
-        course.save()
-        self.supervisor.editCourse(101, "Algebra", "Entry level algebra", "Math")
-        self.assertEqual(Course.objects.filter(courseName="Algebra").count(), 1,
-                         msg="Course was not updated in database")
-
     def test_removeCourse(self):
         course = Course(courseID=101, courseName="Algebra", courseDescription="Entry level college algebra",
                         courseDepartment="Math")
@@ -97,35 +101,46 @@ class testCourse(TestCase):
                          msg="A record exists with the primary key that should have been deleted")
 
 
-class testSection(TestCase):
+class testEditCourse(TestCase):
 
     def setUp(self):
         self.user = User("testemail@uwm.edu", "testpassword", "", 1)
         self.user.save()
         self.supervisor = MyUser("testemail@uwm.edu", "testpassword", "", 1)
+        self.course = Course(courseID=101, courseName="name", courseDescription="desc", courseDepartment="dept")
+        self.course.save()
+
+    def test_editCourseName(self):
+        self.supervisor.editCourse(101, "Algebra", "Entry level algebra", "Math")
+        self.assertEqual(Course.objects.filter(courseName="Algebra").count(), 1,
+                         msg="Course was not updated in database")
+
+    def test_editCourseDesc(self):
+        self.supervisor.editCourse(101, "Algebra", "An entry level college math class", "dept")
+        self.assertEqual(Course.objects.filter(courseDescription="An entry level college math class").count(), 1,
+                         msg="No course exists with the given description")
+
+    def test_editCourseDept(self):
+        self.supervisor.editCourse(101, "Algebra", "desc", "Math")
+        self.assertEqual(Course.objects.filter(courseDepartment="Math").count(), 1,
+                         msg="No such course with in the Math dept")
+
+class addRemoveSection(TestCase):
+
+    def setUp(self):
+        self.user = User("testemail@uwm.edu", "testpassword", "", 1)
+        self.user.save()
+        self.course = Course(courseID=251, courseName="Intermediate Computer Programming",
+                             courseDescription="A more advanced class that covers "
+                                               "programming topics in greater "
+                                               "depth", courseDepartment="CS")
+        self.course.save()
+        self.supervisor = MyUser("testemail@uwm.edu", "testpassword", "", 1)
 
     def test_addSection(self):
-        course = Course(courseID=251, courseName="Intermediate Computer Programming",
-                        courseDescription="A more advanced class that covers "
-                                          "programming topics in greater "
-                                          "depth", courseDepartment="CS")
-        course.save()
-        self.supervisor.addSection(800, "EMS100", "05:00PM", "07:00PM", 30, self.user, course)
+        self.supervisor.addSection(800, "EMS100", "05:00PM", "07:00PM", 30, self.user, self.course)
         self.assertEqual(Section.objects.filter(sectionID=800).count(), 1,
                          msg="No section exists with the given sectionID")
-
-    def test_editSection(self):
-        course = Course(courseID=251, courseName="Intermediate Computer Programming",
-                        courseDescription="A more advanced class that covers "
-                                          "programming topics in greater "
-                                          "depth", courseDepartment="CS")
-        course.save()
-        section = Section(sectionID=800, location="EMS100", startTime="05:00PM", endTime="07:00PM", capacity=30,
-                          TA=self.user, courseID=course)
-        section.save()
-        self.supervisor.editSection(800, "EMS120", "05:00PM", "07:00PM", 30, self.user, course)
-        self.assertEqual(Section.objects.filter(location="EMS120").count(), 1,
-                         msg="The section location was not changed")
 
     def test_removeSection(self):
         course = Course(courseID=251, courseName="Intermediate Computer Programming",
@@ -138,3 +153,57 @@ class testSection(TestCase):
         self.supervisor.removeSection(800)
         self.assertEqual(Section.objects.filter(sectionID=800).count(), 0,
                          msg="A section exists with the given sectionID")
+
+
+class editSection(TestCase):
+    def setUp(self):
+        self.user = User("testemail@uwm.edu", "testpassword", "", 1)
+        self.user.save()
+        self.course = Course(courseID=251, courseName="Intermediate Computer Programming",
+                             courseDescription="A more advanced class that covers "
+                                               "programming topics in greater "
+                                               "depth", courseDepartment="CS")
+        self.course.save()
+        self.section = Section(sectionID=800, location="EMS100", startTime="05:00PM", endTime="07:00PM", capacity=30,
+                          TA=self.user, courseID=self.course)
+        self.section.save()
+        self.supervisor = MyUser("testemail@uwm.edu", "testpassword", "", 1)
+
+    def test_editLocation(self):
+        self.supervisor.editSection(self.section.sectionID, "EMS120", self.section.startTime, self.section.endTime,
+                                    self.section.capacity, self.user, self.course)
+        self.assertEqual(Section.objects.filter(location="EMS120").count(), 1,
+                         msg="The section location was not changed")
+
+    def test_editStartTime(self):
+        self.supervisor.editSection(self.section.sectionID, self.section.location, "02:00PM", self.section.endTime,
+                                    self.section.capacity, self.user, self.course)
+        self.assertEqual(Section.objects.filter(startTime="02:00PM").count(), 1,
+                         msg="The section location was not changed")
+
+    def test_editEndTime(self):
+        self.supervisor.editSection(self.section.sectionID, self.section.location, self.section.startTime, "07:30PM",
+                                    self.section.capacity, self.user, self.course)
+        self.assertEqual(Section.objects.filter(endTime="07:30PM").count(), 1,
+                         msg="The section location was not changed")
+
+    def test_editCapacity(self):
+        self.supervisor.editSection(self.section.sectionID, self.section.location, self.section.startTime, self.section.endTime,
+                                    20, self.user, self.course)
+        self.assertEqual(Section.objects.filter(capacity=20).count(), 1,
+                         msg="The section location was not changed")
+
+    def test_editTA(self):
+        newUser = User("taemail@uwm.edu", "tapass", "", 3)
+        newUser.save()
+        self.supervisor.editSection(self.section.sectionID, self.section.location, self.section.startTime, self.section.endTime,
+                                    20, newUser, self.course)
+        self.assertEqual(Section.objects.filter(TA=newUser).count(), 1,
+                         msg="The section location was not changed")
+
+    def test_newCourseID(self):
+        newCourse = Course(361, "Intro to Software Development", "A class that teaches the basics of software development", "CS")
+        newCourse.save()
+        self.supervisor.editSection(self.section.sectionID, self.section.location, self.section.startTime, self.section.endTime,
+                                    self.section.capacity, self.section.TA, newCourse)
+        self.assertEqual(Section.objects.filter(courseID = newCourse).count(), 1, msg="No section exists with the new courseID")
