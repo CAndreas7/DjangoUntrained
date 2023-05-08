9d493ceed87b5955446022a655c8fb31a172577c
Ethan Nieskes <nieskeee8164@uwec.edu>
2023-05-08 15:17:43 -0500

    updated tests and models, lots of validation stuff added

--- Management/test/unit_tests/test_user.py
+++ Management/test/unit_tests/test_user.py
@@ -1,16 +1,27 @@
 import unittest
-from Management.views import MyUser
+from django.core.exceptions import ObjectDoesNotExist
 from django.test import TestCase
 from Management.models import *
 
 
+
+# For whomever reads this:
+# assertRaises can take in a tuple of exceptions rather than just asserting 1
+# However, in order to determine WHICH exception is being raised, we split the tests
+# To test, we can have if statements and use the "raise MYEXCEPTION" function
+# Or we can HANDLE an exception to continue running
+# LOOK AT THE LINK CHASE SENT
+# https://www.geeksforgeeks.org/emailfield-django-models/
+# line 266 in views
+# In models, methods below initializer
+
 class TestUser(TestCase):
 
     def setUp(self):
-        self.user = User("testemail@uwm.edu", "Wayne", "Bruce", "testpassword", "", 1)
+        self.user = User("testemail@uwm.edu", "test", "experiment", "testpassword", "", 1)
         self.user.save()
-        self.supervisor = MyUser("testemail@uwm.edu", "Kent", "Clark", "testpassword", "", 1)
-
+        self.supervisor = User("testemail@uwm.edu", "Visor", "Super", "password", "", 1)
+        self.supervisor.save()
         # self.instructor = MyUser("secondemail@uwm.edu", "secret", "", 2)
         # self.ta = MyUser("taemail@uwm.edu", "mypass", "", 3)
 
@@ -21,23 +32,6 @@ class TestUser(TestCase):
     def test_setEmail(self):
         self.supervisor.setEmail("myemail@uwm.edu")
         self.assertEqual(self.supervisor.email, "myemail@uwm.edu", msg="Username was not changed")
-
-    def test_getlName(self):
-        name = self.user.getlName()
-        self.assertEqual(self.user.lName, name, msg="Returned last name was not the user's last name")
-
-    def test_setlName(self):
-        self.user.setlName("Dickinson")
-        self.assertEqual("Dickinson", self.user.lName, msg="Last name was not set correctly.")
-
-    def test_getfName(self):
-        name = self.user.getfName()
-        self.assertEqual(self.user.fName, name, msg="Returned first name was not the user's first name")
-
-    def test_setfName(self):
-
-        self.user.setfName("Thomas")
-        self.assertEqual("Thomas", self.user.fName, msg="First name was not set correctly.")
 
     def test_getPassword(self):
         password = self.supervisor.getPassword()
@@ -59,37 +53,94 @@ class TestUser(TestCase):
 class testAccount(TestCase):
 
     def setUp(self):
-        self.user = User("testemail@uwm.edu", "testpassword", "", 1)
+        self.user = User("testemail@uwm.edu", "test", "experiment", "testpassword", "", 1)
         self.user.save()
-        self.supervisor = MyUser("testemail@uwm.edu", "testpassword", "", 1)
+        self.supervisor = User("testemail@uwm.edu", "super", "visor", "password", "", 1)
+        self.supervisor.save()
 
-    def test_addAccount(self):
-        self.supervisor.addAccount("email", "pass", "phone", 3)
-        self.assertEqual(User.objects.filter(email="email").count(), 1, msg="No user exists with the given email")
+    def test_addAccount1(self):
+        self.supervisor.addAccount("email@uwm.edu", "User", "New", "pass", "333-499-2791", 3)
+        self.assertEqual(User.objects.filter(email="email@uwm.edu").count(), 1, msg="No user exists with the given email")
 
-    def test_removeAccount(self):
-        user = User(email="deleteme@uwm.edu", password="goodbye", phone="", role=3)
+    def test_addAccount2(self):
+        self.supervisor.addAccount("testemail@uwm.edu", "Aid", "Teaching", "testpassword", "433-291-1173", 1)
+        self.assertEqual(User.objects.filter(email="testemail@uwm.edu").count(), 1, msg="this account already exists and should not have bee")
+
+    def test_removeAccount1(self):
+        user = User(email="deleteme@uwm.edu", lName="Bye", fName="Good", password="goodbye", phone="111-867-5309", role=3)
         user.save()
         self.supervisor.removeAccount("deleteme@uwm.edu")
         self.assertEqual(User.objects.filter(email="deleteme@uwm.edu", password="goodbye").count(), 0,
                          msg="An account exists with the credentials that should have been deleted")
 
+    def test_removeAccount2(self):
+        with self.assertRaises(ObjectDoesNotExist, msg="No such account exists"):
+            self.supervisor.removeAccount("myfakeemail@uwm.edu")
 
 class editAccount(TestCase):
 
     def setUp(self):
-        self.user = User("testemail@uwm.edu", "testpassword", "", 1)
+        self.user = User("testemail@uwm.edu", "test", "experiment", "testpassword", "", 1)
         self.user.save()
-        self.supervisor = MyUser("testemail@uwm.edu", "testpassword", "", 1)
+        self.supervisor = User("testemail@uwm.edu", "Visor", "Super", "password", "", 1)
+        self.supervisor.save()
+
+    def test_TypeEmail1(self):
+        with self.assertRaises(ValidationError, msg="email cannot be None, fails to raise TypeError"):
+            self.supervisor.setEmail(None)
+
+    def test_TypeEmail2(self):
+        with self.assertRaises(ValidationError, msg="email not a char type data, fails to raise TypeError"):
+            self.supervisor.setEmail(1)
+
+    def test_typeEmail3(self):
+        with self.assertRaises(ValidationError, msg="email not a char type data, fails to raise TypeError"):
+            self.supervisor.setEmail(self.supervisor)
+
+    def test_typeEmail4(self):
+        with self.assertRaises(ValidationError, msg="Email must end with @uwm.edu"):
+            self.supervisor.setEmail("email")
+
+    def test_typePass1(self):
+        with self.assertRaises(ValidationError, msg="Password cannot be None, fails to raise TypeError"):
+            self.supervisor.setPassword(None)
+
+    def test_typePass2(self):
+        with self.assertRaises(ValidationError, msg="Password cannot be integer"):
+            self.supervisor.setPassword(1)
+
+    def test_typePass3(self):
+        with self.assertRaises(ValidationError, msg="Password cannot be empty"):
+            self.supervisor.setPassword("")
 
     def test_editPassword(self):
         self.supervisor.editAccount(self.supervisor.email, "changedpass", "1(888)-999-0000", 3)
         self.assertEqual(User.objects.filter(password="changedpass").count(), 1,
                          msg="Editted field did not match desired field")
 
+    def test_typePhone1(self):
+        with self.assertRaises(ValidationError, msg="Phone field cannot be None"):
+            self.supervisor.setPhone(None)
+
+    def test_typePhone2(self):
+        with self.assertRaises(ValidationError, msg="Phone field cannot be an integer, must be a string"):
+            self.supervisor.setPhone(12324498383)
+
     def test_editPhone(self):
         self.supervisor.editAccount(self.supervisor.email, self.supervisor.password, "1(449)-844-2323", 1)
         self.assertEqual(User.objects.filter(phone="1(449)-844-2323").count(), 1, msg="No account with given phone")
+
+    def test_typeRole1(self):
+        with self.assertRaises(ValidationError, msg="Field cannot be none"):
+            self.supervisor.setRole(None)
+
+    def test_typeRole2(self):
+        with self.assertRaises(ValueError, msg="Role must be in range 1-3"):
+            self.supervisor.setRole(0)
+
+    def test_typeRole3(self):
+        with self.assertRaises(ValueError, msg="Role must be in range 1-3"):
+            self.supervisor.setRole(4)
 
     def test_editRole(self):
         self.supervisor.editAccount(self.supervisor.email, self.supervisor.password, self.supervisor.phone, 2)
@@ -99,9 +150,10 @@ class editAccount(TestCase):
 class testCourseAddRemove(TestCase):
 
     def setUp(self):
-        self.user = User("testemail@uwm.edu", "testpassword", "", 1)
+        self.user = User("testemail@uwm.edu", "test", "experiment", "testpassword", "", 1)
         self.user.save()
-        self.supervisor = MyUser("testemail@uwm.edu", "testpassword", "", 1)
+        self.supervisor = User("testemail@uwm.edu", "Visor", "Super", "password", "", 1)
+        self.supervisor.save()
 
     def test_addCourse(self):
         course = Course(courseID=101, courseName="name", courseDescription="desc", courseDepartment="dept")
@@ -122,9 +174,10 @@ class testCourseAddRemove(TestCase):
 class testEditCourse(TestCase):
 
     def setUp(self):
-        self.user = User("testemail@uwm.edu", "testpassword", "", 1)
+        self.user = User("testemail@uwm.edu", "test", "experiment", "testpassword", "", 1)
         self.user.save()
-        self.supervisor = MyUser("testemail@uwm.edu", "testpassword", "", 1)
+        self.supervisor = User("testemail@uwm.edu", "Visor", "Super", "password", "", 1)
+        self.supervisor.save()
         self.course = Course(courseID=101, courseName="name", courseDescription="desc", courseDepartment="dept")
         self.course.save()
 
@@ -143,17 +196,19 @@ class testEditCourse(TestCase):
         self.assertEqual(Course.objects.filter(courseDepartment="Math").count(), 1,
                          msg="No such course with in the Math dept")
 
+
 class addRemoveSection(TestCase):
 
     def setUp(self):
-        self.user = User("testemail@uwm.edu", "testpassword", "", 1)
+        self.user = User("testemail@uwm.edu", "lastName", "firstName", "testpassword", "222-349-8283", 1)
         self.user.save()
         self.course = Course(courseID=251, courseName="Intermediate Computer Programming",
                              courseDescription="A more advanced class that covers "
                                                "programming topics in greater "
                                                "depth", courseDepartment="CS")
         self.course.save()
-        self.supervisor = MyUser("testemail@uwm.edu", "testpassword", "", 1)
+        self.supervisor = User("testemail@uwm.edu", "Visor", "Super", "testpassword", "123-499-6279", 1)
+        self.supervisor.save()
 
     def test_addSection(self):
         self.supervisor.addSection(800, "EMS100", "05:00PM", "07:00PM", 30, self.user, self.course)
@@ -175,7 +230,7 @@ class addRemoveSection(TestCase):
 
 class editSection(TestCase):
     def setUp(self):
-        self.user = User("testemail@uwm.edu", "testpassword", "", 1)
+        self.user = User("testemail@uwm.edu", "test", "experiment", "testpassword", "", 1)
         self.user.save()
         self.course = Course(courseID=251, courseName="Intermediate Computer Programming",
                              courseDescription="A more advanced class that covers "
@@ -183,9 +238,10 @@ class editSection(TestCase):
                                                "depth", courseDepartment="CS")
         self.course.save()
         self.section = Section(sectionID=800, location="EMS100", startTime="05:00PM", endTime="07:00PM", capacity=30,
-                          TA=self.user, courseID=self.course)
+                               TA=self.user, courseID=self.course)
         self.section.save()
-        self.supervisor = MyUser("testemail@uwm.edu", "testpassword", "", 1)
+        self.supervisor = User("testemail@uwm.edu", "visor", "super", "testpassword", "", 1)
+        self.supervisor.save()
 
     def test_editLocation(self):
         self.supervisor.editSection(self.section.sectionID, "EMS120", self.section.startTime, self.section.endTime,
@@ -197,31 +253,37 @@ class editSection(TestCase):
         self.supervisor.editSection(self.section.sectionID, self.section.location, "02:00PM", self.section.endTime,
                                     self.section.capacity, self.user, self.course)
         self.assertEqual(Section.objects.filter(startTime="02:00PM").count(), 1,
-                         msg="The section location was not changed")
+                         msg="The start time was not changed")
 
     def test_editEndTime(self):
         self.supervisor.editSection(self.section.sectionID, self.section.location, self.section.startTime, "07:30PM",
                                     self.section.capacity, self.user, self.course)
         self.assertEqual(Section.objects.filter(endTime="07:30PM").count(), 1,
-                         msg="The section location was not changed")
+                         msg="The end time was not changed")
 
     def test_editCapacity(self):
-        self.supervisor.editSection(self.section.sectionID, self.section.location, self.section.startTime, self.section.endTime,
+        self.supervisor.editSection(self.section.sectionID, self.section.location, self.section.startTime,
+                                    self.section.endTime,
                                     20, self.user, self.course)
         self.assertEqual(Section.objects.filter(capacity=20).count(), 1,
-                         msg="The section location was not changed")
+                         msg="The capacity was not changed")
 
     def test_editTA(self):
-        newUser = User("taemail@uwm.edu", "tapass", "", 3)
+        newUser = User("taemail@uwm.edu", "Assistant", "Teaching", "tapass", "292-661-7322", 3)
         newUser.save()
-        self.supervisor.editSection(self.section.sectionID, self.section.location, self.section.startTime, self.section.endTime,
+        self.supervisor.editSection(self.section.sectionID, self.section.location, self.section.startTime,
+                                    self.section.endTime,
                                     20, newUser, self.course)
         self.assertEqual(Section.objects.filter(TA=newUser).count(), 1,
-                         msg="The section location was not changed")
+                         msg="The TA was not changed")
 
     def test_newCourseID(self):
-        newCourse = Course(361, "Intro to Software Development", "A class that teaches the basics of software development", "CS")
+        newCourse = Course(361, "Intro to Software Development",
+                           "A class that teaches the basics of software development", "CS")
         newCourse.save()
-        self.supervisor.editSection(self.section.sectionID, self.section.location, self.section.startTime, self.section.endTime,
+        self.supervisor.editSection(self.section.sectionID, self.section.location, self.section.startTime,
+                                    self.section.endTime,
                                     self.section.capacity, self.section.TA, newCourse)
-        self.assertEqual(Section.objects.filter(courseID = newCourse).count(), 1, msg="No section exists with the new courseID")
+        self.assertEqual(Section.objects.filter(courseID=newCourse).count(), 1,
+                         msg="No section exists with the new courseID")
+
