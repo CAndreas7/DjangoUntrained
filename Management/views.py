dbdd753b764cbb07be6c0bfd9b69ba0913fd0495
Chase Andreasen <andreas7@uwm.edu>
2023-05-09 17:47:01 -0500

    removed MyUser

--- Management/views.py
+++ Management/views.py
@@ -148,7 +148,6 @@ class usersInCourse(View):
         for y in usersToCourses:
             users.append(y.getUser())
 
-
         sortedUsers = sorted(users, key=lambda user: (user.role, user.lName))
         # sortedUsers = sorted(users, key=lambda x: (x[6], x[2]))
         context = {'course': course, 'users': sortedUsers}
@@ -260,104 +259,6 @@ class sectionDelete(View):
         return redirect('sections', course_id=course_id)
 
 
-class MyUser(User):
-
-    def __init__(self, email, lName, fName, password, phone, role):
-        self.email = email
-        self.lName = lName
-        self.fName = fName
-        self.password = password
-        self.phone = phone
-        self.role = role
-
-    def addAccount(self, email, password, phone, role):
-        user = User(email=email, password=password, phone=phone, role=role)
-        user.save()
-
-    def getEmail(self):
-        return self.email
-
-    def setEmail(self, email):
-        users = User.objects.filter(email=self.email)
-        for user in users:
-            user.email = email
-            user.save()
-        self.email = email
-
-    def getPassword(self):
-        return self.password
-
-    def setPassword(self, password):
-        passwords = User.objects.filter(password=self.password)
-        for word in passwords:
-            word.password = password
-            word.save()
-        self.password = password
-
-    def getPhone(self):
-        return self.phone
-
-    def setPhone(self, phoneNum):
-        phones = User.objects.filter(phone=phoneNum)
-        for item in phones:
-            item.phone = phoneNum
-            item.save()
-        self.phone = phoneNum
-
-    def editAccount(self, email, password, phoneNum, role):
-        user = User.objects.filter(email=email)
-        for item in user:
-            item.password = password
-            item.phone = phoneNum
-            item.role = role
-            item.save()
-        self.password = password
-        self.phone = phoneNum
-        self.role = role
-
-    def removeAccount(self, email):
-        User.objects.filter(email=email).delete()
-        self.email = None
-        self.password = None
-        self.phone = None
-        self.role = None
-
-    def addCourse(self, courseID, courseName, courseDesc, courseDept):
-        course = Course(courseID=courseID, courseName=courseName, courseDescription=courseDesc,
-                        courseDepartment=courseDept)
-        course.save()
-
-    def editCourse(self, courseID, courseName, courseDescription, courseDepartment):
-        courses = Course.objects.filter(courseID=courseID)
-        for item in courses:
-            item.courseName = courseName
-            item.courseDescription = courseDescription
-            item.courseDepartment = courseDepartment
-            item.save()
-
-    def removeCourse(self, courseID):
-        Course.objects.filter(courseID=courseID).delete()
-
-    def addSection(self, sectionID, location, startTime, endTime, capacity, ta, courseID):
-        section = Section(sectionID=sectionID, location=location, startTime=startTime, endTime=endTime,
-                          capacity=capacity, TA=ta, courseID=courseID)
-        section.save()
-
-    def editSection(self, sectionID, location, startTime, endTime, capacity, ta, courseID):
-        sections = Section.objects.filter(sectionID=sectionID)
-        for item in sections:
-            item.location = location
-            item.startTime = startTime
-            item.endTime = endTime
-            item.capacity = capacity
-            item.TA_id = ta
-            item.courseID_id = courseID
-            item.save()
-
-    def removeSection(self, sectionID):
-        Section.objects.filter(sectionID=sectionID).delete()
-
-
 class userAdd(View):
     def get(self, request):
         form = UserForm()
@@ -433,6 +334,7 @@ class userDelete(View):
         user = Course.objects.all()
         # context = {'results': user, 'roleTemplate': userRole, 'message': "Account Successfully Deleted"}
         return render(request, "main/User/users.html")
+
 
 class notificationSend(View):
     # I think down the road we may not need this. For example, when adding a user to course or section,
